164f23721f4e1d885e5cb1bf01e5b4205bbbf439
AustinT33 <austintumlinson@gmail.com>
2020-01-29 11:31:39 -0600

    drills 3 & 4

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,5 @@
+import Queue from "./Queue";
+
 class BinarySearchTree {
     constructor(key = null, value = null, parent = null) {
         this.key = key;
@@ -93,6 +95,37 @@ class BinarySearchTree {
             return this;
         }
         return this.left._findMin();
+    }
+
+    dfs(values=[]) {
+        if(this.left) {
+            values = this.left.dfs(values);
+        }
+        values.push(this.value);
+
+        if (this.right) {
+            values = this.right.dfs(values);
+        }
+        return values;
+    }
+
+    bfs(tree, values = []) {
+        const queue = new Queue();
+        const node = tree.root;
+        queue.enqueue(node);
+        while (queue.length) {
+            const node = queue.dequeue()
+            values.push(node.value);
+
+            if(node.left) {
+                queue.enqueue(node.left)
+            }
+
+            if(node.right) {
+                queue.enqueue(node.right);
+            }
+        }
+        return values;
     }
 }
 
--- src/drills.js
+++ src/drills.js
@@ -9,3 +9,24 @@ const sequenceTo8 = binarySearch(arr1, 8, 0, arr1.length - 1).path;
 const sequenceTo16 = binarySearch(arr2, 16, 0, arr2.length - 1).path;
 
 console.log(sequenceTo8, sequenceTo16);
+
+
+// 3. find a book
+
+// function find(array, value, title) {
+//  for (let i = 0; i < array.length; i++) {
+//      if (array[i] === value) {
+//        return i;
+//      }
+//    }
+//      return title[i];
+//  }
+//          O(n)
+
+
+// 4. searching in a bst
+
+// 14, 19, 15, 27, 25, 89, 91, 90, 79, 35
+// 14, 19, 15, 27, 25, 91, 90, 79, 89, 35
+
+// 8, 6, 5, 7, 10, 9, 11
